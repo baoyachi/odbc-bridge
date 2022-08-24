914601bf93df019536f604ba00f3489000e39e6f
baoyachi <liaoymxsdl@gmail.com>
2022-08-24 18:05:50 +0800

    add table print

--- src/debug.rs
+++ src/debug.rs
@@ -1,37 +1,59 @@
-use std::error::Error;
-use std::io::stdout;
-use odbc_api::{Cursor, CursorImpl, ResultSetMetadata};
+use nu_protocol::Config;
+use nu_table::{Alignments, StyledString, Table, TableTheme, TextStyle};
 use odbc_api::buffers::TextRowSet;
 use odbc_api::handles::StatementImpl;
+use odbc_api::{Cursor, CursorImpl, ResultSetMetadata};
+use std::collections::HashMap;
+use std::error::Error;
 
 const BATCH_SIZE: usize = 5000;
 
-pub fn print_all_tables(mut cursor: CursorImpl<StatementImpl<'_>>) -> Result<(), Box<dyn Error>> {
-    let out = stdout();
-    let mut writer = csv::Writer::from_writer(out);
-    // Write the column names to stdout
-    let headline: Vec<String> = cursor.column_names()?.collect::<Result<_, _>>()?;
-    writer.write_record(headline)?;
+pub fn print_all_tables(cursor: CursorImpl<StatementImpl<'_>>) -> Result<(), Box<dyn Error>> {
+    let table = convert_table(cursor)?;
+    print_table(table);
+    Ok(())
+}
+
+pub fn convert_table(mut cursor: CursorImpl<StatementImpl<'_>>) -> Result<Table, Box<dyn Error>> {
+    let headers: Vec<StyledString> = cursor
+        .column_names()?
+        .collect::<Result<Vec<String>, _>>()?
+        .into_iter()
+        .map(|x| StyledString::new(x, TextStyle::default_header()))
+        .collect();
 
     // Use schema in cursor to initialize a text buffer large enough to hold the largest
     // possible strings for each column up to an upper limit of 4KiB.
     let mut buffers = TextRowSet::for_cursor(BATCH_SIZE, &mut cursor, Some(4096))?;
     // Bind the buffer to the cursor. It is now being filled with every call to fetch.
     let mut row_set_cursor = cursor.bind_buffer(&mut buffers)?;
-
+    let mut rows = vec![];
     // Iterate over batches
     while let Some(batch) = row_set_cursor.fetch()? {
         // Within a batch, iterate over every row
         for row_index in 0..batch.num_rows() {
             // Within a row iterate over every column
-            let record = (0..batch.num_cols()).map(|col_index| {
-                batch
-                    .at(col_index, row_index)
-                    .unwrap_or(&[])
-            });
-            // Writes row as csv
-            writer.write_record(record)?;
+            let row_data = (0..batch.num_cols())
+                .map(|col_index| batch.at(col_index, row_index).unwrap_or(&[]))
+                .into_iter()
+                .map(|x| String::from_utf8_lossy(x).to_string())
+                .map(|x| StyledString::new(x, TextStyle::basic_left()))
+                .collect();
+            rows.push(row_data);
         }
     }
-    Ok(())
+    let table = Table::new(headers, rows, TableTheme::rounded());
+
+    Ok(table)
+}
+
+fn print_table(table: Table) {
+    let cfg = Config::default();
+    let styles = HashMap::default();
+    let alignments = Alignments::default();
+
+    let string = table
+        .draw_table(&cfg, &styles, alignments, usize::MAX)
+        .unwrap();
+    println!("{}", string);
 }
