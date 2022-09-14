6ae856f1c9cde3f339464a2fcad2bd6a5530c059
baoyachi <liaoymxsdl@gmail.com>
2022-09-14 10:44:02 +0800

    bump odbc-api-helper new version 0.1.12

--- src/extension/pg.rs
+++ src/extension/pg.rs
@@ -159,3 +159,40 @@ impl From<QueryResult> for PgQueryResult {
         }
     }
 }
+
+impl Convert<PgType> for Oid {
+    fn convert(self) -> PgType {
+        PgType::from_oid(self).unwrap()
+    }
+}
+
+impl Convert<PgType> for PgType {
+    fn convert(self) -> PgType {
+        self
+    }
+}
+
+pub fn oid_typlen<C: Convert<PgType>>(c: C) -> i16 {
+    let pg_type = c.convert();
+    match pg_type {
+        PgType::BOOL => 1,
+        PgType::BYTEA => -1,
+        PgType::CHAR => 1,
+        PgType::INT8 => 8,
+        PgType::INT2 => 2,
+        PgType::INT2_VECTOR => -1,
+        PgType::INT4 => 4,
+        PgType::TEXT => -1,
+        PgType::FLOAT4 => 4,
+        PgType::FLOAT8 => 8,
+        PgType::VARCHAR => -1,
+        PgType::DATE => 4,
+        PgType::TIME => 8,
+        PgType::TIMESTAMP => 8,
+        PgType::TIMESTAMPTZ => 8,
+        PgType::TIMETZ => 12,
+        PgType::BIT => -1,
+        PgType::JSONB => -1,
+        _ => panic!("unknow pg_type:{}", pg_type),
+    }
+}
