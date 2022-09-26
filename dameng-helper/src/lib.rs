b6b87abae85054d501b630ae3d463b391c5d2e74
baoyachi <liaoymxsdl@gmail.com>
2022-09-26 11:13:14 +0800

    add DataType

--- dameng-helper/src/lib.rs
+++ dameng-helper/src/lib.rs
@@ -1,16 +1,3 @@
-mod dm_type;
+#[allow(non_camel_case_types)]
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod data_type;
