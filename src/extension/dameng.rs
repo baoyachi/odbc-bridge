56a0eb22ec8c36b5b3d94d631dd4806418165db4
baoyachi <liaoymxsdl@gmail.com>
2022-10-10 14:37:51 +0800

    change dameng show table sql

--- src/extension/dameng.rs
+++ src/extension/dameng.rs
@@ -1,1 +1,11 @@
+use dameng_helper::table::DmTableDesc;
+use crate::executor::table::TableDescResult;
+use crate::TryConvert;
 
+impl TryConvert<DmTableDesc> for TableDescResult{
+    type Error = anyhow::Error;
+
+    fn try_convert(self) -> Result<DmTableDesc, Self::Error> {
+        DmTableDesc::new(self.0,self.1)
+    }
+}
