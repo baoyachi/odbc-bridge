5b5e5bc4893195be9fcf78383b40bd5184d82c37
baoyachi <liaoymxsdl@gmail.com>
2022-10-20 19:20:03 +0800

    use host call image server

--- init_test.sh
+++ init_test.sh
@@ -1,5 +1,5 @@
 echo "start exec isql test"
-while ! nc -z dm8_single 5236;
+while ! nc -z localhost 5236;
 do
   echo "wait for dm8_single";
   sleep 1;
@@ -8,5 +8,4 @@ done;
 echo "dm8_single is ready!";
 echo "start web service here";
 env
-
 ~/.cargo/bin/cargo test
