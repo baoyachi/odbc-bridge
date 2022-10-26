59e8ac859c26cf07be5515f756881524d9fe2dd5
baoyachi <liaoymxsdl@gmail.com>
2022-10-26 16:56:51 +0800

    add unit test

--- dameng-helper/src/lib.rs
+++ dameng-helper/src/lib.rs
@@ -109,6 +109,21 @@ mod tests {
     use once_cell::sync::Lazy;
     use regex::Regex;
 
+    #[macro_export]
+    macro_rules! svec {
+            () => {{
+                let v = Vec::<String>::new();
+                v
+
+            }};
+            ($($elem:expr),+ $(,)?) => {{
+                let v = vec![
+                    $( String::from($elem), )*
+                ];
+                v
+            }};
+        }
+
     pub static ENV: Lazy<Environment> = Lazy::new(|| Environment::new().unwrap());
 
     /// Validate dameng database crtdate datetime format value
@@ -136,7 +151,7 @@ mod tests {
             conn,
             Options::new("SYSDBA".to_string(), SupportDatabase::Dameng),
         )
-        .unwrap();
+            .unwrap();
         connection
     }
 
@@ -210,53 +225,523 @@ CREATE TABLE SYSDBA.T4 (
 	"role" TEXT NOT NULL,
 	"source" TEXT NOT NULL
 );"#;
-        let exec_result: ExecResult = connection.execute(create_table_t4).unwrap();
-        assert_eq!(exec_result.rows_affected, 0);
+        let table_desc_fn = |connection: OdbcDbConnection<'_>| {
+            let exec_result: ExecResult = connection.execute(create_table_t4).unwrap();
+            assert_eq!(exec_result.rows_affected, 0);
 
-        //2. query table
-        let mut table_desc = connection
-            .show_table(vec!["T2".to_string(), "T3".to_string(), "T4".to_string()])
-            .unwrap();
+            //2. query table
+            let mut table_desc = connection
+                .show_table(vec!["T2".to_string(), "T3".to_string(), "T4".to_string()])
+                .unwrap();
 
-        let _: Vec<_> = table_desc
-            .1
-            .iter_mut()
-            .map(|x| {
-                // id must greater than 0
+            let _: Vec<_> = table_desc
+                .1
+                .iter_mut()
+                .map(|x| {
+                    let len = x.len();
+                    let id = x.get(1).unwrap().parse::<usize>().unwrap();
+                    // id must greater than 0
+                    assert!(id > 0);
 
-                let len = x.len();
-                let id = x.get(1).unwrap().parse::<usize>().unwrap();
-                assert!(id > 0);
+                    // validate CRTDATE value:2022-10-24 17:28:26.308000
+                    let crtdate = x.last().unwrap();
+                    assert_eq!(true, validate_crtdate(crtdate));
+                    let _ = std::mem::replace(&mut x[1], "1058".to_string());
+                    let _ =
+                        std::mem::replace(&mut x[len - 1], "2022-10-24 17:28:26.308000".to_string());
+                    x
+                })
+                .collect();
+            table_desc
+        };
 
-                // validate CRTDATE value:2022-10-24 17:28:26.308000
-                let crtdate = x.last().unwrap();
-                assert_eq!(true, validate_crtdate(crtdate));
-                let _ = std::mem::replace(&mut x[1], "1058".to_string());
-                let _ =
-                    std::mem::replace(&mut x[len - 1], "2022-10-24 17:28:26.308000".to_string());
-                x
-            })
-            .collect();
+        let table_desc = table_desc_fn(connection);
+        // test Options case_sensitive:false
+        assert_eq!(table_desc, mock_table_result_uppercase());
 
-        assert_eq!(table_desc, mock_table_result())
+        // change Options case_sensitive:true
+        let mut connection = get_dameng_conn();
+        connection.options.case_sensitive = true;
+        let table_desc = table_desc_fn(connection);
+        info!("{}",serde_json::from_str(&table_desc).unwrap());
+        assert_eq!(table_desc, mock_table_result());
     }
 
+    pub fn mock_table_result_uppercase() -> TableDescResult {
+        let headers = svec![
+            "NAME",
+            "ID",
+            "COLID",
+            "TYPE$",
+            "LENGTH$",
+            "SCALE",
+            "NULLABLE$",
+            "DEFVAL",
+            "TABLE_NAME",
+            "CRTDATE"
+        ];
+
+        let datas = vec![
+            svec![
+                "C1",
+                "1058",
+                "0",
+                "DATETIME WITH TIME ZONE",
+                "10",
+                "6",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "C2",
+                "1058",
+                "1",
+                "TIMESTAMP",
+                "8",
+                "6",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "C3",
+                "1058",
+                "2",
+                "VARCHAR",
+                "100",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "C4",
+                "1058",
+                "3",
+                "NUMERIC",
+                "0",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "C5",
+                "1058",
+                "4",
+                "TIME WITH TIME ZONE",
+                "7",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "C6",
+                "1058",
+                "5",
+                "TIMESTAMP WITH LOCAL TIME ZONE",
+                "8",
+                "4102",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "NUMBER",
+                "1058",
+                "6",
+                "NUMBER",
+                "0",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "DECIMAL",
+                "1058",
+                "7",
+                "DECIMAL",
+                "0",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "BIT",
+                "1058",
+                "8",
+                "BIT",
+                "1",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "INTEGER",
+                "1058",
+                "9",
+                "INTEGER",
+                "4",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "XXX_PLS_INTEGER",
+                "1058",
+                "10",
+                "INTEGER",
+                "4",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "BIGINT",
+                "1058",
+                "11",
+                "BIGINT",
+                "8",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "TINYINT",
+                "1058",
+                "12",
+                "TINYINT",
+                "1",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "BYTE",
+                "1058",
+                "13",
+                "BYTE",
+                "1",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "SMALLINT",
+                "1058",
+                "14",
+                "SMALLINT",
+                "2",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "BINARY",
+                "1058",
+                "15",
+                "BINARY",
+                "1",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "VARBINARY",
+                "1058",
+                "16",
+                "VARBINARY",
+                "8188",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "REAL",
+                "1058",
+                "17",
+                "REAL",
+                "4",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "FLOAT",
+                "1058",
+                "18",
+                "FLOAT",
+                "8",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "DOUBLE",
+                "1058",
+                "19",
+                "DOUBLE",
+                "8",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "DOUBLE_PRECISION",
+                "1058",
+                "20",
+                "DOUBLE PRECISION",
+                "8",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "CHAR",
+                "1058",
+                "21",
+                "CHAR",
+                "1",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "VARCHAR",
+                "1058",
+                "22",
+                "VARCHAR",
+                "8188",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "TEXT",
+                "1058",
+                "23",
+                "TEXT",
+                "2147483647",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "IMAGE",
+                "1058",
+                "24",
+                "IMAGE",
+                "2147483647",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "BLOB",
+                "1058",
+                "25",
+                "BLOB",
+                "2147483647",
+                "0",
+                "Y",
+                "",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "NOT_NULL_TEST",
+                "1058",
+                "26",
+                "VARCHAR",
+                "100",
+                "0",
+                "N",
+                "'default_value_hh'",
+                "T2",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "NOT_NULL_TEST_LEN",
+                "1058",
+                "27",
+                "VARCHAR",
+                "100",
+                "0",
+                "N",
+                "'default_value_hh'",
+                "T2",
+                "2022-10-24 17:28:26.308000",
+            ],
+            svec![
+                "C1",
+                "1058",
+                "0",
+                "DATETIME WITH TIME ZONE",
+                "10",
+                "6",
+                "Y",
+                "",
+                "T3",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "CASE_SENSITIVE",
+                "1058",
+                "1",
+                "TIMESTAMP",
+                "8",
+                "6",
+                "Y",
+                "",
+                "T3",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "C3",
+                "1058",
+                "2",
+                "VARCHAR",
+                "100",
+                "0",
+                "Y",
+                "",
+                "T3",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "C4",
+                "1058",
+                "3",
+                "NUMERIC",
+                "0",
+                "0",
+                "Y",
+                "",
+                "T3",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "NOT_NULL_TEST_LEN",
+                "1058",
+                "4",
+                "VARCHAR",
+                "100",
+                "0",
+                "N",
+                "'default_value_hh'",
+                "T3",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "ID",
+                "1058",
+                "0",
+                "INT",
+                "4",
+                "0",
+                "N",
+                "",
+                "T4",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "USER_ID",
+                "1058",
+                "1",
+                "CHARACTER VARYING",
+                "8188",
+                "0",
+                "N",
+                "",
+                "T4",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "USER_NAME",
+                "1058",
+                "2",
+                "TEXT",
+                "2147483647",
+                "0",
+                "N",
+                "",
+                "T4",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "ROLE",
+                "1058",
+                "3",
+                "TEXT",
+                "2147483647",
+                "0",
+                "N",
+                "",
+                "T4",
+                "2022-10-24 17:28:26.308000"
+            ],
+            svec![
+                "SOURCE",
+                "1058",
+                "4",
+                "TEXT",
+                "2147483647",
+                "0",
+                "N",
+                "",
+                "T4",
+                "2022-10-24 17:28:26.308000"
+            ],
+        ];
+        (headers, datas)
+    }
     pub fn mock_table_result() -> TableDescResult {
-        #[macro_export]
-        macro_rules! svec {
-            () => {{
-                let v = Vec::<String>::new();
-                v
-
-            }};
-            ($($elem:expr),+ $(,)?) => {{
-                let v = vec![
-                    $( String::from($elem), )*
-                ];
-                v
-            }};
-        }
-
         let headers = svec![
             "NAME",
             "ID",
