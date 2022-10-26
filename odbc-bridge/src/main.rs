013877d15b332eb2b8812aef00e931b7750fcfc8
baoyachi <liaoymxsdl@gmail.com>
2022-10-26 18:32:54 +0800

    add odbc-bridge

---
+++ odbc-bridge/src/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+use serde::{Deserialize, Serialize};
+use odbc_api_helper::executor::database::{OdbcDbConnection, Options};
+use odbc_api_helper::executor::SupportDatabase;
+use odbc_api_helper::odbc_api::Environment;
+use odbc_api_helper::print_all_tables;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    path: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EnvConfig {
+    connection: String,
+    database: String,
+    sql: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let json = fs::read_to_string(args.path).unwrap();
+    let config: EnvConfig = serde_json::from_str(&json).unwrap();
+    println!("config:{:?}",config);
+    let env = Environment::new().unwrap();
+    let conn = env.connect_with_connection_string(&config.connection)
+        .unwrap();
+
+    let connection = OdbcDbConnection::new(
+        conn,
+        Options::new(config.database.to_string(), SupportDatabase::Dameng),
+    )
+        .unwrap();
+    let cursor_impl = connection.conn.execute(&config.sql, ()).unwrap().unwrap();
+    print_all_tables(cursor_impl).unwrap();
+}
