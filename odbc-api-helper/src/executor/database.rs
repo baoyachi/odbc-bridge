6ea8dd117a5f8c0040f717e2f1b650e98d520830
gamife <gamife9886@gmail.com>
2022-12-14 07:18:47 +0000

    feat: long text

--- odbc-api-helper/src/executor/database.rs
+++ odbc-api-helper/src/executor/database.rs
@@ -6,6 +6,8 @@ use crate::executor::query::QueryResult;
 use crate::executor::statement::StatementInput;
 use crate::executor::table::{TableDescArgsString, TableDescResult};
 use crate::executor::SupportDatabase;
+use crate::extension::odbc::OdbcColsBuf;
+use crate::extension::odbc::OdbcDataBuf;
 use crate::extension::odbc::{OdbcColumnDesc, OdbcColumnItem, OdbcParamDesc};
 use crate::{Convert, TryConvert};
 use dameng_helper::DmAdapter;
@@ -242,6 +244,33 @@ impl<'a> OdbcDbConnection<'a> {
         let columns: Vec<OdbcColumnDesc> = Self::get_cursor_columns(&mut cursor)?;
 
         debug!("columns:{:?}", columns);
+        if OdbcDataBuf::is_long_data(
+            columns.iter(),
+            self.options.max_str_len,
+            self.options.max_binary_len,
+        ) {
+            let mut cols_buf = OdbcColsBuf::try_from_col_desc(
+                &columns,
+                self.options.max_str_len,
+                self.options.max_binary_len,
+            )?;
+
+            let mut stmt_ref =
+                odbc_common::odbc_api::handles::AsStatementRef::as_stmt_ref(&mut cursor);
+
+            cols_buf.bind_col(&mut stmt_ref)?;
+
+            let mut total_row = vec![];
+
+            while let Some(mut cursor_row) = cursor.next_row()? {
+                total_row.push(cols_buf.get_row(&mut cursor_row)?);
+            }
+
+            return Ok(QueryResult {
+                columns,
+                data: total_row,
+            });
+        }
 
         let descs = columns.iter().map(|c| {
             <(&OdbcColumnDesc, &Options) as TryConvert<BufferDesc>>::try_convert((c, &self.options))
--- odbc-api-helper/src/extension/odbc.rs
+++ odbc-api-helper/src/extension/odbc.rs
@@ -1,13 +1,16 @@
 use crate::executor::database::Options;
+use crate::executor::query::OdbcRow;
 use crate::{Convert, TryConvert};
 use bytes::BytesMut;
-use odbc_common::odbc_api::handles::ParameterDescription;
+use odbc_common::error::{OdbcStdError, OdbcStdResult};
+use odbc_common::odbc_api::buffers::Indicator;
+use odbc_common::odbc_api::handles::{ParameterDescription, Statement, StatementRef};
 use odbc_common::odbc_api::{
     buffers::{AnySlice, BufferDesc},
     sys::{Date, Time, Timestamp, NULL_DATA},
     DataType,
 };
-use odbc_common::odbc_api::{ColumnDescription, Nullability};
+use odbc_common::odbc_api::{Bit, ColumnDescription, CursorRow, Nullability, Nullable};
 use std::cmp::min;
 
 #[derive(Debug, Clone)]
@@ -95,6 +98,347 @@ impl TryConvert<BufferDesc> for (&OdbcColumnDesc, &Options) {
         }
 
         Ok(desc)
+    }
+}
+
+pub struct OdbcColsBuf {
+    pub columns: Vec<(u16, OdbcDataBuf)>,
+}
+
+impl OdbcColsBuf {
+    pub fn try_from_col_desc(
+        col_desc: &[OdbcColumnDesc],
+        max_str_len: usize,
+        max_binary_len: usize,
+    ) -> OdbcStdResult<Self> {
+        let mut columns = Vec::with_capacity(col_desc.len());
+        let mut col_num = 0;
+        for col_desc in col_desc.iter() {
+            let buf =
+                OdbcDataBuf::try_from_data_type(col_desc.data_type, max_str_len, max_binary_len)?;
+            col_num += 1;
+            columns.push((col_num, buf));
+        }
+        Ok(Self { columns })
+    }
+
+    pub fn bind_col(&mut self, cursor: &mut StatementRef<'_>) -> OdbcStdResult<()> {
+        for (col_num, col_buf) in &mut self.columns {
+            unsafe {
+                col_buf.bind_col(*col_num, cursor)?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn get_row(&mut self, cursor_row: &mut CursorRow) -> OdbcStdResult<OdbcRow> {
+        let mut columns = Vec::with_capacity(self.columns.len());
+
+        for (col_num, col_buf) in &mut self.columns {
+            columns.push(col_buf.get_data(*col_num, cursor_row)?)
+        }
+
+        Ok(columns)
+    }
+}
+
+pub enum OdbcDataBuf {
+    Text(Vec<u8>),
+    WText(Vec<u8>),
+    Binary(Vec<u8>),
+    Date(Nullable<Date>),
+    Time(Nullable<Time>),
+    Timestamp(Nullable<Timestamp>),
+    F64(Nullable<f64>),
+    F32(Nullable<f32>),
+    I8(Nullable<i8>),
+    I16(Nullable<i16>),
+    I32(Nullable<i32>),
+    I64(Nullable<i64>),
+    U8(Nullable<u8>),
+    Bit(Nullable<Bit>),
+}
+
+impl OdbcDataBuf {
+    /// # Safety
+    ///
+    /// It is the callers responsibility to make sure the bound columns live until they are no
+    /// longer bound.
+    pub unsafe fn bind_col(
+        &mut self,
+        column_number: u16,
+        cursor: &mut StatementRef<'_>,
+    ) -> OdbcStdResult<()> {
+        match self {
+            OdbcDataBuf::Date(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::Time(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::Timestamp(buf) => {
+                cursor.bind_col(column_number, buf).into_result(cursor)?
+            }
+            OdbcDataBuf::F64(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::F32(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::I8(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::I16(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::I32(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::I64(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::U8(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+            OdbcDataBuf::Bit(buf) => cursor.bind_col(column_number, buf).into_result(cursor)?,
+
+            OdbcDataBuf::Text(_) | OdbcDataBuf::WText(_) | OdbcDataBuf::Binary(_) => {}
+        };
+        Ok(())
+    }
+
+    pub fn get_data(
+        &mut self,
+        column_number: u16,
+        cursor_row: &mut CursorRow,
+    ) -> OdbcStdResult<OdbcColumnItem> {
+        let result = match self {
+            OdbcDataBuf::Date(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::Date,
+                value: buf.as_opt().map(|value| {
+                    bytes::BytesMut::from(value.try_convert().unwrap().to_string().as_bytes())
+                }),
+            },
+            OdbcDataBuf::Time(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::Time,
+                value: buf.as_opt().map(|value| {
+                    bytes::BytesMut::from(value.try_convert().unwrap().to_string().as_bytes())
+                }),
+            },
+            OdbcDataBuf::Timestamp(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::Timestamp,
+                value: buf.as_opt().map(|value| {
+                    bytes::BytesMut::from(
+                        <Timestamp as TryConvert<time::PrimitiveDateTime>>::try_convert(*value)
+                            .unwrap()
+                            .to_string()
+                            .as_bytes(),
+                    )
+                }),
+            },
+            OdbcDataBuf::F64(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::F64,
+                value: buf
+                    .as_opt()
+                    .map(|value| bytes::BytesMut::from(value.to_string().as_bytes())),
+            },
+            OdbcDataBuf::F32(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::F32,
+                value: buf
+                    .as_opt()
+                    .map(|value| bytes::BytesMut::from(value.to_string().as_bytes())),
+            },
+            OdbcDataBuf::I8(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::I8,
+                value: buf
+                    .as_opt()
+                    .map(|value| bytes::BytesMut::from(value.to_string().as_bytes())),
+            },
+            OdbcDataBuf::I16(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::I16,
+                value: buf
+                    .as_opt()
+                    .map(|value| bytes::BytesMut::from(value.to_string().as_bytes())),
+            },
+            OdbcDataBuf::I32(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::I32,
+                value: buf
+                    .as_opt()
+                    .map(|value| bytes::BytesMut::from(value.to_string().as_bytes())),
+            },
+            OdbcDataBuf::I64(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::I64,
+                value: buf
+                    .as_opt()
+                    .map(|value| bytes::BytesMut::from(value.to_string().as_bytes())),
+            },
+            OdbcDataBuf::U8(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::U8,
+                value: buf
+                    .as_opt()
+                    .map(|value| BytesMut::from(vec![*value].as_slice())),
+            },
+            OdbcDataBuf::Bit(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::Bit,
+                value: buf
+                    .as_opt()
+                    .map(|value| BytesMut::from(value.as_bool().to_string().as_bytes())),
+            },
+
+            OdbcDataBuf::Text(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::Text,
+                value: Self::get_long_text(column_number, buf, cursor_row)?,
+            },
+
+            OdbcDataBuf::WText(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::WText,
+                value: Self::get_long_text(column_number, buf, cursor_row)?,
+            },
+            OdbcDataBuf::Binary(buf) => OdbcColumnItem {
+                odbc_type: crate::extension::odbc::OdbcColumnType::Binary,
+                value: Self::get_long_binary(column_number, buf, cursor_row)?,
+            },
+        };
+        Ok(result)
+    }
+
+    pub fn get_long_text(
+        column_number: u16,
+        buf: &mut [u8],
+        cursor_row: &mut CursorRow,
+    ) -> OdbcStdResult<Option<BytesMut>> {
+        let mut actual = Option::<bytes::BytesMut>::None;
+        let mut target: odbc_common::odbc_api::parameter::VarCharSliceMut<'_> =
+            odbc_common::odbc_api::parameter::VarChar::from_buffer(buf, Indicator::Null);
+        loop {
+            cursor_row.get_data(column_number, &mut target)?;
+            if let Some(v) = target.as_bytes() {
+                match actual {
+                    Some(ref mut actual) => {
+                        actual.extend_from_slice(v);
+                    }
+                    None => {
+                        actual = Some(bytes::BytesMut::from(v));
+                    }
+                }
+            }
+
+            if target.is_complete() {
+                break;
+            }
+        }
+        Ok(actual)
+    }
+    pub fn get_long_binary(
+        column_number: u16,
+        buf: &mut [u8],
+        cursor_row: &mut CursorRow,
+    ) -> OdbcStdResult<Option<BytesMut>> {
+        let mut actual = Option::<bytes::BytesMut>::None;
+        let mut target: odbc_common::odbc_api::parameter::VarBinarySliceMut<'_> =
+            odbc_common::odbc_api::parameter::VarBinary::from_buffer(buf, Indicator::Null);
+        loop {
+            cursor_row.get_data(column_number, &mut target)?;
+            if let Some(v) = target.as_bytes() {
+                match actual {
+                    Some(ref mut actual) => {
+                        actual.extend_from_slice(v);
+                    }
+                    None => {
+                        actual = Some(bytes::BytesMut::from(v));
+                    }
+                }
+            }
+
+            if target.is_complete() {
+                break;
+            }
+        }
+        Ok(actual)
+    }
+
+    pub fn try_from_data_type(
+        data_type: DataType,
+        max_str_len: usize,
+        max_binary_len: usize,
+    ) -> OdbcStdResult<Self> {
+        Self::from_data_type(data_type, max_str_len, max_binary_len).ok_or_else(|| {
+            OdbcStdError::StringError(format!(
+                "covert DataType:{:?} to OdbcDataBuf error",
+                data_type
+            ))
+        })
+    }
+
+    pub fn from_data_type(
+        data_type: DataType,
+        max_str_len: usize,
+        max_binary_len: usize,
+    ) -> Option<Self> {
+        let result = match data_type {
+            DataType::Numeric { precision, scale } | DataType::Decimal { precision, scale }
+                if scale == 0 && precision < 3 =>
+            {
+                OdbcDataBuf::I8(Nullable::<i8>::null())
+            }
+            DataType::Numeric { precision, scale } | DataType::Decimal { precision, scale }
+                if scale == 0 && precision < 10 =>
+            {
+                OdbcDataBuf::I32(Nullable::<i32>::null())
+            }
+            DataType::Numeric { precision, scale } | DataType::Decimal { precision, scale }
+                if scale == 0 && precision < 19 =>
+            {
+                OdbcDataBuf::I64(Nullable::<i64>::null())
+            }
+            DataType::Integer => OdbcDataBuf::I32(Nullable::<i32>::null()),
+            DataType::SmallInt => OdbcDataBuf::I16(Nullable::<i16>::null()),
+            DataType::Float { precision: 0..=24 } | DataType::Real => {
+                OdbcDataBuf::F32(Nullable::<f32>::null())
+            }
+            DataType::Float { precision: 25..=53 } | DataType::Double => {
+                OdbcDataBuf::F64(Nullable::<f64>::null())
+            }
+            DataType::Date => OdbcDataBuf::Date(Nullable::<Date>::null()),
+            DataType::Time { precision: 0 } => OdbcDataBuf::Time(Nullable::<Time>::null()),
+            DataType::Timestamp { precision: _ } => {
+                OdbcDataBuf::Timestamp(Nullable::<Timestamp>::null())
+            }
+            DataType::BigInt => OdbcDataBuf::I64(Nullable::<i64>::null()),
+            DataType::TinyInt => OdbcDataBuf::I8(Nullable::<i8>::null()),
+            DataType::Bit => OdbcDataBuf::Bit(Nullable::<Bit>::null()),
+            DataType::Varbinary {length }
+            | DataType::Binary { length }
+            | DataType::LongVarbinary { length } => {
+                OdbcDataBuf::Binary(vec![0u8; min(max_binary_len, length) ])
+            }
+
+            DataType::WVarchar { length } | DataType::WChar { length } => {
+                OdbcDataBuf::WText(vec![0u8; min(max_str_len, length) ])
+            }
+
+            DataType::Varchar { length } | DataType::Char { length } | DataType::LongVarchar { length } => {
+                OdbcDataBuf::Text(vec![0u8; min(max_str_len, length) ])
+            }
+            // Specialized buffers for Numeric and decimal are not yet supported.
+            DataType::Numeric {
+                precision: _,
+                scale: _,
+            }
+            | DataType::Decimal {
+                precision: _,
+                scale: _,
+            }
+            | DataType::Time { precision: _ } => OdbcDataBuf::Text(vec![0u8; data_type.display_size().unwrap()]),
+            DataType::Unknown
+            | DataType::Float { precision: _ }
+            | DataType::Other {
+                data_type: _,
+                column_size: _,
+                decimal_digits: _,
+            } => return None,
+        };
+        Some(result)
+    }
+
+    pub fn is_long_data<'a>(
+        mut col_desc: impl Iterator<Item = &'a OdbcColumnDesc>,
+        max_str_len: usize,
+        max_binary_len: usize,
+    ) -> bool {
+        col_desc.any(|i| match i.data_type {
+            odbc_common::odbc_api::DataType::Char { length }
+            | odbc_common::odbc_api::DataType::WChar { length }
+            | odbc_common::odbc_api::DataType::Varchar { length }
+            | odbc_common::odbc_api::DataType::WVarchar { length }
+            | odbc_common::odbc_api::DataType::LongVarchar { length } => length > max_str_len,
+            odbc_common::odbc_api::DataType::LongVarbinary { length }
+            | odbc_common::odbc_api::DataType::Varbinary { length }
+            | odbc_common::odbc_api::DataType::Binary { length } => length > max_binary_len,
+            _ => false,
+        })
     }
 }
 
