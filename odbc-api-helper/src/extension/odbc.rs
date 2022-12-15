a9e1261afbcf5c69dcea0a399f05c08d9ccce37f
gamife <gamife9886@gmail.com>
2022-12-15 07:53:55 +0000

    fix: string contain 0x00

--- odbc-api-helper/src/extension/odbc.rs
+++ odbc-api-helper/src/extension/odbc.rs
@@ -5,13 +5,14 @@ use bytes::BytesMut;
 use odbc_common::error::{OdbcStdError, OdbcStdResult};
 use odbc_common::odbc_api::buffers::Indicator;
 use odbc_common::odbc_api::handles::{ParameterDescription, Statement, StatementRef};
+use odbc_common::odbc_api::parameter::{VarChar, VarCharSliceMut};
 use odbc_common::odbc_api::{
     buffers::{AnySlice, BufferDesc},
     sys::{Date, Time, Timestamp, NULL_DATA},
     DataType,
 };
 use odbc_common::odbc_api::{Bit, ColumnDescription, CursorRow, Nullability, Nullable};
-use std::cmp::min;
+use std::cmp::{max, min};
 
 #[derive(Debug, Clone)]
 pub struct OdbcColumnDesc {
@@ -99,6 +100,34 @@ impl TryConvert<BufferDesc> for (&OdbcColumnDesc, &Options) {
 
         Ok(desc)
     }
+}
+
+// https://tools.ietf.org/html/rfc3629
+const UTF8_CHAR_WIDTH: &[u8; 256] = &[
+    // 1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 0
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 1
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 2
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 3
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 4
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 5
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 6
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 7
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 8
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 9
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // A
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // B
+    0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, // C
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, // D
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, // E
+    4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // F
+];
+
+/// Given a first byte, determines how many bytes are in this UTF-8 character.
+#[must_use]
+#[inline]
+pub const fn utf8_char_width(b: u8) -> usize {
+    UTF8_CHAR_WIDTH[b as usize] as usize
 }
 
 pub struct OdbcColsBuf {
@@ -303,30 +332,86 @@ impl OdbcDataBuf {
         //   第一次返回 33个中文*3 = 99byte, 第100位是odbc规定截断的字符串后面要接 0x00(The null-termination character), 而101和102位都不会被驱动更改,
         //   第二次返回 1个中文*3 = 3 byte, 除了前3个byte,后续的其他位都不会被修改
 
-        // 通过查看target.is_complete()的判断条件可以知道, buf的最后一个u8必须要是0.
-        if let Some(last) = buf.last_mut() {
-            *last = 0;
-        }
+        const INVALID_UTF8_BYTE: u8 = 0xFF;
+        let reset_buf = |buffer: &mut [u8]| {
+            // buf_length 比 data_length 大1才不会截断.
+            // 截断是按照字符而不是字节去截断的, 一个utf8最多占4个字节, 所以可能buf里的倒数第234字节, 总共三个字节没有被使用到(倒数第一个字节总是0).
+            // 通过把最后234字节赋值未一个不可能出现在utf8的字节(0xff), 来判断此次获取的数据长度
+            if let Some(end) = buffer.rchunks_mut(4).next() {
+                for i in end {
+                    *i = INVALID_UTF8_BYTE;
+                }
+            }
+            // 通过查看target.is_complete()的判断条件可以知道, buf的最后一个u8必须要是0.
+            if let Some(last) = buffer.last_mut() {
+                *last = 0;
+            }
+        };
 
         let mut actual = Option::<bytes::BytesMut>::None;
-        let mut target: odbc_common::odbc_api::parameter::VarCharSliceMut<'_> =
-            odbc_common::odbc_api::parameter::VarChar::from_buffer(buf, Indicator::Null);
 
+        let mut last_time_is_zero = false;
         loop {
+            reset_buf(buf);
+            let mut target: VarCharSliceMut<'_> = VarChar::from_buffer(buf, Indicator::Null);
+
             cursor_row.get_data(column_number, &mut target)?;
-            if let Some(mut v) = target.as_bytes() {
+
+            if let Some(mut data_without_zero) = target.as_bytes() {
+                let mut current_is_zero = false;
                 if !target.is_complete() {
-                    if let Some(prefix) = v.splitn(2, |num| *num == 0).next() {
-                        v = prefix;
-                    };
+                    let mut trim_len = 0;
+
+                    if let Some(end) = data_without_zero.rchunks(3).next() {
+                        let mut iter = end.iter().rev();
+                        if let Some(last) = iter.next() {
+                            match *last {
+                                0 => {
+                                    current_is_zero = true;
+                                }
+                                INVALID_UTF8_BYTE => {
+                                    trim_len += 1;
+                                    for item in iter {
+                                        if *item == INVALID_UTF8_BYTE {
+                                            trim_len += 1;
+                                        } else {
+                                            // 驱动会在实际返回数据的后一字节设置0, 不一定是buf的最后一个字节, 所以这里还需要移除0
+                                            // 比如,(0x61='a') 最开始:
+                                            // buf = .. 0xFF 0xFF 0xFF 0x00
+                                            // get_data 之后:
+                                            // buf = .. 0x61 0x00 0xFF 0x00
+                                            // data_without_zero = .. 0x61 0x00 0xFF
+                                            // 所以除了移除掉0xFF, 还要移除0x00
+                                            trim_len += 1;
+                                            break;
+                                        }
+                                    }
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+
+                    data_without_zero = &data_without_zero[..data_without_zero.len() - trim_len]
                 }
 
                 match actual {
                     Some(ref mut actual) => {
-                        actual.extend_from_slice(v);
+                        if last_time_is_zero {
+                            // 0可能是驱动设置的null-termination, 也可能是字符串本身包含0x00
+                            // 上一次最后一个字节是0, 且这一次第一个utf8字符是 一个字节的, 说明上一次最后一个字节0是字符串本身包含的.
+                            // 否则这一次的第一个字节本应该放在上一次的最后一个字节处.
+                            if utf8_char_width(data_without_zero[0]) > 1 && actual.len() > 1 {
+                                // 移除null-termination
+                                actual.truncate(actual.len() - 1);
+                            }
+                        }
+                        actual.extend_from_slice(data_without_zero);
+                        last_time_is_zero = current_is_zero;
                     }
                     None => {
-                        actual = Some(bytes::BytesMut::from(v));
+                        actual = Some(bytes::BytesMut::from(data_without_zero));
+                        last_time_is_zero = current_is_zero;
                     }
                 }
             }
@@ -383,6 +468,7 @@ impl OdbcDataBuf {
         max_str_len: usize,
         max_binary_len: usize,
     ) -> Option<Self> {
+        const UTF8_MAX_BYTE: usize = 4;
         let result = match data_type {
             DataType::Numeric { precision, scale } | DataType::Decimal { precision, scale }
                 if scale == 0 && precision < 3 =>
@@ -422,14 +508,14 @@ impl OdbcDataBuf {
             }
 
             DataType::WVarchar { length } | DataType::WChar { length } => {
-                OdbcDataBuf::WText(vec![0u8; min(max_str_len, length) + 1])
+                OdbcDataBuf::WText(vec![0u8; max(min(max_str_len, length), UTF8_MAX_BYTE) + 1])
             }
 
             // Plus one, since the null-termination character.
             DataType::Varchar { length }
             | DataType::Char { length }
             | DataType::LongVarchar { length } => {
-                OdbcDataBuf::Text(vec![0u8; min(max_str_len, length) + 1])
+                OdbcDataBuf::Text(vec![0u8; max(min(max_str_len, length), UTF8_MAX_BYTE) + 1])
             }
             // Specialized buffers for Numeric and decimal are not yet supported.
             DataType::Numeric {
